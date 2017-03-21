51d724753de08be009c015f7c8916db93c55cb21
hYang <familyhuangyang@hotmail.com>
2017-03-22 00:16:45 +0800

    ml tree

---
+++ ml/tree.py
@@ -0,0 +1,20 @@
+from math import log
+
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] +=1
+    shannoEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key])/numEntries
+        shannoEnt -= prob*log(prob,2)
+    return shannoEnt
+
+def createDataSet():
+    dataSet = [[1,1,'yes'],[1,0,'yes'],[0,1,'no'],[0,1,'no'],[0,1,'no']]
+    labels = ['no surfacing','flippers']
+    return dataSet,labels
