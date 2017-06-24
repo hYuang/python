add693986acfa90a7775f717d10ac33cd469648b
hYang <familyhuangyang@hotmail.com>
2017-06-24 22:12:27 +0800

    dump the register content

--- ida/my_debugger.py
+++ ida/my_debugger.py
@@ -6,7 +6,10 @@ kernel32 = windll.kernel32
 
 class debugger():
     def __init__(self):
-        pass
+        self.h_process = None
+        self.pid = None
+        self.debugger_active = False
+
     
     def load(self,path_to_exe):
         create_flags =DEBUG_PROCESS
@@ -31,7 +34,85 @@ class debugger():
                                     byref(process_information)):
             print "[*] We have successfully launched the process!"
             print "[*] PID: %d "%process_information.dwProcessId
+            self.h_process = self.open_process(process_information.dwProcessId)
         else:
             print "[*] Error : 0x%08x." %kernel32.GetLastError()
+    
+    def open_process(self,pid):
+        h_process = kernel32.OpenProcess(PROCESS_ALL_ACCESS,False,pid)
+        return h_process
+
+    def attach(self,pid):
+        self.h_process = self.open_process(pid)
+
+        if kernel32.DebugActiveProcess(pid):
+            self.debugger_active = True
+            self.pid = int(pid)
+            #self.run()
+        else:
+            print "[*] Unbale to attach to the process"
+    
+    def run(self):
+        
+        while self.debugger_active == True:
+            self.get_debug_event()
+
+    def get_debug_event(self):
+        debug_event = DEBUG_EVENT()
+        continue_status = DBG_CONTINUE
+
+        if kernel32.WaitForDebugEvent(byref(debug_event),INFINITE):
+            raw_input("press a key to continue ... ")
+            self.debugger_active = False
+            kernel32.ContinueDebugEvent(debug_event.dwProcessId,
+                                        debug_event.dwProcessId,
+                                        continue_status)
+    
+    def detach(self):
+        if kernel32.DebugActiveProcessStop(self.pid):
+            print "[*] Finished debugging . Exiting ..."
+            return True
+        else:
+            print "there was an error"
+            return False
+
+
+    def open_thread(self, thread_id):
+            h_thread = kernel32.OpenThread(THREAD_ALL_ACCESS, None, thread_id)
+            if h_thread is not None:
+                return h_thread
+            else:
+                print "[*] Could not obtain a valid thread handle."
+                return False
+	
+    def enumerate_threads(self):
+        thread_entry = THREADENTRY32()
+        thread_list = []
+        snapshot = kernel32.CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, self.pid)
+        if snapshot is not None:
+            thread_entry.dwSize = sizeof(thread_entry)
+            success = kernel32.Thread32First(snapshot,  byref(thread_entry))
+            while success:
+                if thread_entry.th32OwnerProcessID == self.pid:
+                    thread_list.append(thread_entry.th32ThreadID)
+                success = kernel32.Thread32Next(snapshot, byref(thread_entry))
+
+            kernel32.CloseHandle(snapshot)
+            return thread_list
+        else:
+            print "enumerate_threads fail."
+            return False
+
+    def get_thread_context(self, thread_id):
+        context = CONTEXT()
+        context.ContextFlags = CONTEXT_FULL | CONTEXT_DEBUG_REGISTERS
+        h_thread = self.open_thread(thread_id)
+        if kernel32.GetThreadContext(h_thread, byref(context)):
+            kernel32.CloseHandle(h_thread)
+            return context
+        else:
+            print "get_thread_context fail."
+            return False
+        
 
 
--- ida/my_test.py
+++ ida/my_test.py
@@ -1,5 +1,25 @@
+# -*- coding: utf-8 -*-
+# @Date    : 2016-08-12 14:18:10
+# @Author  : giantbranch (giantbranch@gmail.com)
+# @Link    : http://blog.csdn.net/u012763794?viewmode=contents
+
 import my_debugger
-
 debugger = my_debugger.debugger()
+pid = raw_input("Enter the PID of the process to attach to:")
+debugger.attach(int(pid))
 
-debugger.load("C:\\Windows\\System32\\calc.exe")
+threadList = debugger.enumerate_threads()
+print threadList
+for thread in threadList:
+	thread_context = debugger.get_thread_context(thread)
+	print "[*] Dumping registers for thread ID:0x%08x" % thread
+	print "[**] EIP:0x%08x" % thread_context.Eip
+	print "[**] ESP:0x%08x" % thread_context.Esp
+	print "[**] EBP:0x%08x" % thread_context.Ebp
+	print "[**] EAX:0x%08x" % thread_context.Eax
+	print "[**] EBX:0x%08x" % thread_context.Ebx
+	print "[**] ECX:0x%08x" % thread_context.Ecx
+	print "[**] EDX:0x%08x" % thread_context.Edx
+	print "[*] END DUMP"
+
+debugger.detach()
